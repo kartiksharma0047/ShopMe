72f66b616759b2f153706aa35c60cd3bef43b12a
kartiksharma0047 <kartiksharma55109@gmail.com>
2025-01-25 23:53:08 +0530

    Completed

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import Cart from "./page/Cart";
+import Home from "./page/Home";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./page/Login";
+
+import { useSelector } from "react-redux";
+
+function App() {
+  const { user } = useSelector((state) => state.user);
+  document.title = "ShopMe";
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
